a25846ae85d33b8e84f9bab34b48877ad784c0c6
prithasathish <thayaprith10@gmail.com>
2025-10-18 23:23:05 +0530

    changes localhost

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", // local
+      "http://localhost:5173", 
+        "http://localhost:5174",
       "https://vendor-crackers.netlify.app", // deployed frontend
     ],
     credentials: true,
